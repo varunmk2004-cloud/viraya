51935cfa76f30f2fd1fea867acb77d95c7574392
varunmk2004-cloud <varunmk2004@gmail.com>
2026-01-21 21:51:16 +0530

    Initial Files

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const instance = axios.create({ baseURL: 'http://localhost:5000/api' });
+const baseURL = import.meta.env?.VITE_API_URL || 'http://localhost:5000/api';
+const instance = axios.create({ baseURL });
 
 let isRefreshing = false;
 let failedQueue = [];

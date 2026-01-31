700a30330c93ded85a385632701be709c9033f61
varunmk2004-cloud <varunmk2004@gmail.com>
2026-01-31 15:25:57 +0530

    added render api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance = axios.create({ baseURL: 'http://localhost:5000/api' });
+const instance = axios.create({ baseURL: 'https://viraya.onrender.com/api' });
 
 let isRefreshing = false;
 let failedQueue = [];
